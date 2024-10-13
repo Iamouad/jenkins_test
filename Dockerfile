7814606fa058c98a2396cc02481ae7b4a4f28b3d
root <root@ip-172-31-91-113.ec2.internal>
2024-10-13 11:45:29 +0000

    update index.html

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 FROM nginx:latest
 COPY ./index.html /usr/share/nginx/html/index.html
+RUN sed -i 's/today/$(date)/g' /usr/share/nginx/html/index.html
--- rollout-docker.sh
+++ rollout-docker.sh
@@ -5,16 +5,20 @@ echo $1 $2
 cp $1 /root/jenkins_test/
 docker build /root/jenkins_test -t nginx-custom:$2
 #start test container on port 8082
+echo ------------- Starting Test container --------------------
 docker run -ti --rm -d -p 8082:80 --name web-server-test nginx-custom:$2
 #Readiness test
 while [[ $(docker inspect -f json web-server-test |jq -r .[0].State.Status) != running ]]; do
 	sleep 1;
 done
 #Test container test
+echo ------------- Testing Test container --------------------
 curl localhost:8082
 #Remove test container
+echo ------------- Removing Test container --------------------
 docker rm -f web-server-test
-#Update main container
+#Upgrade main container
+echo ------------- Upgrading App container --------------------
 docker rm -f web-server
 docker run -ti --rm -d -p 8081:80 --name web-server nginx-custom:$2
 while [[ $(docker inspect -f json web-server |jq -r .[0].State.Status) != running ]]; do

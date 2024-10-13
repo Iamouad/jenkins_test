0e2321901bbbaf2d44d001cecb2cabae4f52711e
root <root@ip-172-31-91-113.ec2.internal>
2024-10-13 14:07:39 +0000

    resume

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # jenkins_test
 
-Nginx static web server <br>
+## Intro
+This is minimalist Nginx web server thats servers a static web page [https://github.com/Iamouad/jenkins_test/blob/main/index.html](index.html)
+<br>
+
+## Jenkins pipeline
+
+A pipeline is configured in jenkins that listens to push events on the main branch of this project
+
+[https://github.com/Iamouad/jenkins_test/blob/main/rollout-docker.sh] (rollout-docker.sh)
+This bash script does all the stages of the **CI/CD** of the app following  this steps:
+1. Builds a new docker image of the web-server and tags it with the CI_COMMIT TAG
+2. Deploys a test container in order to tets the new image
+3. Upgrades the **main container** to the new version of the app
+4. Rollback to the previous version in case of an upgrade failure
--- rollout-docker.sh
+++ rollout-docker.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 set -e
-echo $1 $2
+#Retrieve the current deployment image version
 current_deployed_tag=$(docker ps -f name=web-server --format json | jq -r .Image)
 cp $1 /root/jenkins_test/
+
+echo ------------- Building Docker --------------------
 docker build --no-cache /root/jenkins_test -t nginx-custom:$2
+
 #start test container on port 8082
 echo ------------- Starting Test container --------------------
 docker rm -f web-server-test || echo "Cleanining up test environement"
 docker run -ti --rm -d -p 8082:80 --name web-server-test nginx-custom:$2
+
 #Readiness test
 i=0
 while [[ $(docker inspect -f json web-server-test |jq -r .[0].State.Status) != running ]]; do
@@ -20,12 +24,15 @@ while [[ $(docker inspect -f json web-server-test |jq -r .[0].State.Status) != r
 	   exit 1
 	fi
 done
-#Test container test
+
+#Test the output of the test container
 echo ------------- Testing Test container --------------------
 curl localhost:8082
-#Remove test container
+
+#Cleanup test environement
 echo ------------- Removing Test container --------------------
 docker rm -f web-server-test
+
 #Upgrade main container
 echo ------------- Upgrading App container --------------------
 docker rm -f web-server
@@ -37,6 +44,7 @@ while [[ $(docker inspect -f json web-server |jq -r .[0].State.Status) != runnin
         if [[ "$i" -gt "5"  ]]; then
            docker rm -f web-server
 	   echo "Timeout!!! Container not running after waiting $i (s)"
+	   #Rollback in case of failure
 	   echo "------------------- Rolling back deployment -------------------"
            docker run -ti --rm -d -p 8081:80 --name web-server $current_deployed_tag
            exit 1
